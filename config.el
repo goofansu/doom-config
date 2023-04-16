0deb65a8d469f66068c9e1ca8bb090531149f196
Yejun Su <yejun@hey.com>
2023-04-16 14:00:59 +0800

    update org

--- config.el
+++ config.el
@@ -271,7 +271,32 @@
   (advice-add 'org-todo           :after (func-ignore #'org-save-all-org-buffers))
   ;; Tracking habits
   (add-to-list 'org-modules 'org-habit)
-  (setq org-habit-show-all-today nil))
+  (setq org-habit-show-all-today nil)
+  ;; See https://github.com/daviwil/dotfiles/blob/master/Emacs.org
+  ;; Increase the size of various headings
+  (set-face-attribute 'org-document-title nil :font "Overpass" :weight 'bold :height 1.3)
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "JetBrains Mono" :weight 'medium :height (cdr face)))
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+  ;; Get rid of the background on column views
+  (set-face-attribute 'org-column nil :background nil)
+  (set-face-attribute 'org-column-title nil :background nil))
 
 (after! org-roam
   (setq org-roam-completion-everywhere t)
