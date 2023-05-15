9cb894b1cf14c909c9e24aefa7f592d984bb77c4
Yejun Su <yejun@hey.com>
2023-05-15 11:06:39 +0800

    Revert "remove unnecessary copilot key bindings"
    This reverts commit 600fddd882954e62aa75ab4fea1b1fd6b23e19fa.

--- config.el
+++ config.el
@@ -166,8 +166,11 @@
 ;; Packages
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
-  :bind (:map copilot-completion-map
-         ("<tab>" . 'copilot-accept-completion)))
+  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
+         ("C-<tab>" . 'copilot-accept-completion-by-word)
+         :map copilot-completion-map
+         ("<tab>" . 'copilot-accept-completion)
+         ("TAB" . 'copilot-accept-completion)))
 
 (use-package! elixir-mode
   :hook (before-save . elixir-format-before-save)
