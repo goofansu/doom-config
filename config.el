261ce649ac60240cdf42bdba87a6fd8e8ce79988
Yejun Su <yejun@hey.com>
2023-07-23 11:43:15 +0800

    set auth-source

--- config.el
+++ config.el
@@ -7,7 +7,9 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Yejun Su"
-      user-mail-address "yejun@hey.com")
+      user-mail-address "yejun@hey.com"
+      auth-sources '("~/.authinfo.gpg")
+      auth-source-cache-expiry nil)
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
