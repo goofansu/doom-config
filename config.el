f8b86d51e56862ca5e80587f2582dfbfff42e741
Yejun Su <yejun@hey.com>
2023-07-23 11:08:33 +0800

    update format

--- config.el
+++ config.el
@@ -86,8 +86,8 @@
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
               ("TAB" . 'copilot-accept-completion)
+              ("<tab>" . 'copilot-accept-completion)
               ("C-TAB" . 'copilot-accept-completion-by-word)
               ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
