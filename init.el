4ac0852c2b9b7abded29a4ddda12a99e6a22649e
Yejun Su <yejun@hey.com>
2023-08-09 12:25:54 +0800

    enable pass module

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
        (magit            ; a git porcelain for Emacs
         +forge)
        make              ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;terraform         ; infrastructure as code
