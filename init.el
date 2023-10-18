dc5235ceb014532abad4a83f2c8af0c125f30b3f
Yejun Su <goofan.su@gmail.com>
2023-10-18 11:43:40 +0800

    enable rest module

--- init.el
+++ init.el
@@ -166,7 +166,7 @@
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
-       ;;rest              ; Emacs as a REST client
+       (rest +jq)          ; Emacs as a REST client
        ;;rst               ; ReST in peace
        (ruby             ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
         +rails
