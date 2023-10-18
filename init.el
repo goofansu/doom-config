48601c881170078a2ac8f89e2ab2599937306bb1
Yejun Su <goofan.su@gmail.com>
2023-10-18 11:51:39 +0800

    format

--- init.el
+++ init.el
@@ -156,8 +156,8 @@
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
-        +pandoc
         +hugo
+        +pandoc
         +present)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
